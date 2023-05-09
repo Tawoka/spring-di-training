b3749b8f6e81a62ca9313558e772f65a4d2e7a19
tawoka <github@spectral-darkness.org>
2023-05-09 11:06:38 +0200

    Doing DI in the unit test

--- src/test/java/com/rmorgner/di/DependencyInjectionApplicationTests.java
+++ src/test/java/com/rmorgner/di/DependencyInjectionApplicationTests.java
@@ -1,13 +1,34 @@
 package com.rmorgner.di;
 
+import com.rmorgner.di.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class DependencyInjectionApplicationTests {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
+	@Autowired
+	private MyController controller;
+
+	@Test
+	void testGetControllerFromContext(){
+		MyController controller = applicationContext.getBean(MyController.class);
+		System.out.println(controller.sayHello());
+	}
+
+	@Test
+	void testInjectedController(){
+		System.out.println(controller.sayHello());
+	}
+
 	@Test
 	void contextLoads() {
+
 	}
 
 }
