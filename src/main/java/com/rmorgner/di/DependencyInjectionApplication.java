0efd5680895d70402a12388a567b5b3ff3d41ea8
tawoka <github@spectral-darkness.org>
2023-05-09 11:01:58 +0200

    setting up basic injector and retrieving instance of class

--- src/main/java/com/rmorgner/di/DependencyInjectionApplication.java
+++ src/main/java/com/rmorgner/di/DependencyInjectionApplication.java
@@ -1,13 +1,21 @@
 package com.rmorgner.di;
 
+import com.rmorgner.di.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DependencyInjectionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DependencyInjectionApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(DependencyInjectionApplication.class, args);
+
+		MyController controller = context.getBean(MyController.class);
+
+		System.out.println("I'm in the main method");
+
+		System.out.println(controller.sayHello());
 	}
 
 }
