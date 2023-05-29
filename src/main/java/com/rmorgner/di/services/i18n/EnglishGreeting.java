162025afa603c6259713e6575f4bac86b6c8d7b4
tawoka <github@spectral-darkness.org>
2023-05-29 14:37:14 +0200

    default profile

--- src/main/java/com/rmorgner/di/services/i18n/EnglishGreeting.java
+++ src/main/java/com/rmorgner/di/services/i18n/EnglishGreeting.java
@@ -4,7 +4,7 @@ import com.rmorgner.di.services.GreetingService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN", "default"})
 @Service("i18nService")
 public class EnglishGreeting implements GreetingService {
   @Override
